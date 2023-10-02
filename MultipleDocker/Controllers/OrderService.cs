d0694828a04b9106f3171de68163bbd679bcac42
EmilS <emilstephansen@hotmail.com>
2023-10-02 10:33:50 +0200

    Updated compose file

--- MultipleDocker/Controllers/OrderService.cs
+++ MultipleDocker/Controllers/OrderService.cs
@@ -10,20 +10,22 @@ namespace MultipleDocker.Controllers
         public async Task<ActionResult<string>> Get()
         {
             HttpClient client = new HttpClient();
-            string urlAdress = Environment.GetEnvironmentVariable("urlAdress");
-            if (urlAdress == null)
+            string urlAdress = Environment.GetEnvironmentVariable("urlsForExternal");
+            List<string> urls = urlAdress.Split(";").ToList();
+            foreach (string url in urls)
             {
-                return BadRequest("Missing urlAdress!");
-            }
-            var resp = await client.GetAsync("http://"+urlAdress);
-            if (!resp.IsSuccessStatusCode)
-            {
-                return BadRequest($"cant connect {urlAdress}");
-            }
-            else
-            {
-                var ans = await resp.Content.ReadAsStringAsync();
-                Console.WriteLine(ans);
+                var resp = await client.GetAsync("http://" + url);
+                if (!resp.IsSuccessStatusCode)
+                {
+
+                    var bAns = await resp.Content.ReadAsStringAsync();
+                    return BadRequest($"Error : {bAns} while connecting to: {url}");
+                }
+                else
+                {
+                    var ans = await resp.Content.ReadAsStringAsync();
+                    Console.WriteLine(ans);
+                }
             }
             //keep asking other services...
             return Ok("all the things did the thing");
